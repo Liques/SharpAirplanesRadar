9e2183773c1217986c7dfb33a614be842cf01b50
Liques <liques@hotmail.com>
2020-02-24 14:31:31 +0000

    Bug fix related to load resources.

--- NiceAirplanesRadar/Util/ResourceHelper.cs
+++ NiceAirplanesRadar/Util/ResourceHelper.cs
@@ -11,12 +11,20 @@ namespace NiceAirplanesRadar.Util
         private const string resourceFoldelLocal = "";
         public static string LoadExternalResource(string fileName)
         {
+            var maxFileExpiration = new TimeSpan(15, 0, 0);
+
             string splitter = System.Runtime.InteropServices.RuntimeInformation.IsOSPlatform(System.Runtime.InteropServices.OSPlatform.Windows) ? "\\" : "/";
-             
+
             var fileLocation = fileName;
             string fileContent = String.Empty;
 
-            if (!File.Exists(fileLocation)  && System.IO.File.GetLastWriteTime(fileName).AddDays(15) < DateTime.Now)
+            if (File.Exists(fileLocation) && System.IO.File.GetLastWriteTime(fileName).Add(maxFileExpiration) >= DateTime.Now)
+            {
+                var file = File.OpenText(fileLocation);
+                fileContent = file.ReadToEnd();
+                file.Close();
+            }
+            else
             {
                 try
                 {
@@ -25,24 +33,18 @@ namespace NiceAirplanesRadar.Util
                     HttpResponseMessage response = null;
 
                     response = httpClient.GetAsync(resourceFolderUrl + fileName).Result;
+                    fileContent = response.Content.ReadAsStringAsync().Result;
 
-                    //var newfile = File.Create(fileLocation);
-                 
-                    File.WriteAllText(fileLocation,response.Content.ReadAsStringAsync().Result);
-                    //newfile.Close();
+                    File.WriteAllText(fileLocation, fileContent);
+
                     LoggingHelper.LogBehavior($">> File '{fileName}' dowloaded and saved from server.");
-                    
-
                 }
                 catch (Exception e)
                 {
                     throw new ArgumentException("Error trying to download resource from server.", e);
                 }
-            }
 
-            var file = File.OpenText(fileLocation);
-            fileContent = file.ReadToEnd();
-            file.Close();
+            }
 
             LoggingHelper.LogBehavior($">> Starting to converting file '{fileName}'...");
 
